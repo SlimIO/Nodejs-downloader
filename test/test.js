38c001a9f5b360334db9173003b0ed00ae2218c8
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-11-29 06:36:58 +0100

    test: add some tests

--- test/test.js
+++ test/test.js
@@ -1,19 +1,114 @@
+// Require Node.js Dependencies
 const {
-    downloadNodeFile,
-    getNodeRelease,
-    constants: { File }
-} = require("../index");
+    promises: { access, unlink, stat },
+    constants: { R_OK }
+} = require("fs");
 
-const { createGunzip } = require("zlib");
-const { createReadStream } = require("fs");
-const { join } = require("path");
+// Require Third-party Dependencies
+const ava = require("ava");
+const is = require("@slimio/is");
+const rimraf = require("rimraf");
 
-const TEMP = join(__dirname, "..", "temp");
+// Require Internal Dependencies
+const Downloader = require("../index");
+const File = Downloader.constants.File;
 
-async function main() {
-    const file = await downloadNodeFile("v11.1.0", File.WinExe64, TEMP);
+ava("Downloader should export constants", (assert) => {
+    assert.true(Reflect.has(Downloader, "constants"));
+    const constants = Downloader.constants;
 
-    createReadStream(file)
-        .pipe(createGunzip());
-}
-main().catch(console.error);
+    assert.is(Object.keys(constants).length, 1);
+    assert.true(Reflect.has(constants, "File"));
+    assert.true(Object.isFrozen(constants.File));
+
+    for (const value of Object.values(constants.File)) {
+        assert.true(typeof value === "string");
+    }
+});
+
+ava("getNodeRelease - version must be a string", async(assert) => {
+    const error = await assert.throwsAsync(Downloader.getNodeRelease(5), TypeError);
+    assert.is(error.message, "version should be typeof string");
+});
+
+ava("getNodeRelease - unknown version must return undefined", async(assert) => {
+    const release = await Downloader.getNodeRelease("v8.500.0");
+    assert.is(release, undefined);
+});
+
+ava("getNodeRelease - validate payload", async(assert) => {
+    const release = await Downloader.getNodeRelease(process.version);
+
+    assert.is(release.version, process.version);
+    assert.true(is.string(release.name));
+    assert.true(is.date(release.date));
+    assert.true(is.set(release.files));
+    for (const file of release.files) {
+        assert.true(is.string(file));
+    }
+    assert.true(is.string(release.npm));
+    assert.true(is.string(release.v8));
+    assert.true(is.string(release.zlib));
+    assert.true(is.string(release.openssl));
+    assert.true(is.string(release.uv));
+    assert.true(is.number(release.modules));
+    assert.true(is.bool(release.lts));
+});
+
+ava("getNodeRelease - get non-lts version", async(assert) => {
+    const release = await Downloader.getNodeRelease("v9.0.0");
+
+    assert.is(release.version, "v9.0.0");
+    assert.is(release.lts, false);
+});
+
+ava("downloadNodeFile - fileName must be a string", async(assert) => {
+    const error = await assert.throwsAsync(Downloader.downloadNodeFile(5), TypeError);
+    assert.is(error.message, "fileName must be a string!");
+});
+
+ava("downloadNodeFile - version must be a string", async(assert) => {
+    const opt = {
+        version: 10
+    };
+    const error = await assert.throwsAsync(Downloader.downloadNodeFile(File.Headers, opt), TypeError);
+    assert.is(error.message, "version must be a string");
+});
+
+ava("downloadNodeFile - destination must be a string", async(assert) => {
+    const opt = {
+        dest: 10
+    };
+    const error = await assert.throwsAsync(Downloader.downloadNodeFile(File.Headers, opt), TypeError);
+    assert.is(error.message, "destination must be a string");
+});
+
+ava("downloadNodeFile - Download Node.js headers", async(assert) => {
+    const tarFile = await Downloader.downloadNodeFile(File.Headers, {
+        dest: __dirname
+    });
+
+    await access(tarFile, R_OK);
+    await unlink(tarFile);
+    assert.pass();
+});
+
+ava("extract tar.gz file", async(assert) => {
+    const tarFile = await Downloader.downloadNodeFile(File.Headers, {
+        version: "v9.0.0",
+        dest: __dirname
+    });
+
+    await access(tarFile, R_OK);
+    const dirName = await Downloader.extract(tarFile);
+    console.log(dirName);
+    await unlink(tarFile);
+    const stats = await stat(dirName);
+    assert.true(stats.isDirectory());
+
+    rimraf(dirName, (error) => {
+        if (error) {
+            console.error(error);
+        }
+    });
+});
